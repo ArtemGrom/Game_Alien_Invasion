cff8bbc520b5e402aa314d00320a9397565e1fb9
AG <art.gromov@bk.ru>
2021-07-21 16:46:18 +0300

    rendering of the ship and surface

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings():
+    """Class for storing all game setting"""
+
+    def __init__(self):
+        """Initialization a game setting"""
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
