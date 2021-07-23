d13714b27af7fd7f7c9942e084a9334c2ad544cd
AG <art.gromov@bk.ru>
2021-07-23 16:14:09 +0300

    add rocket

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,10 +11,11 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     while True:  # Tracking keyboard and mouse events
-        gf.check_events()
+        gf.check_events(ship)
+        ship.update()
         gf.update_screen(ai_settings, screen, ship)
 
 
--- game_functions.py
+++ game_functions.py
@@ -1,15 +1,37 @@
 import sys
 import pygame
 
-def check_events():
+
+def check_events(ship):
     """Handles keystrokes and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ship)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
 
 
 def update_screen(ai_settings, screen, ship):
-    """Обновляет изображения на экране и отображает новый экран."""
+    """Refreshes the images on the screen and displays the new screen."""
     screen.fill(ai_settings.bg_color)  # redraws the screen on each pass of the loop
     ship.blitme()
     pygame.display.flip()  # Show of the last drawn screen
+
+
+def check_keydown_events(event, ship):
+    """Реагирует на нажатие клавиш."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+    """Реагирует на отпускание клавиш."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+    
