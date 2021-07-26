483265c8bc54e49f51a2a6b26d9fbf465513f66d
AG <art.gromov@bk.ru>
2021-07-26 15:15:25 +0300

    create fleet aliens v2.0

--- settings.py
+++ settings.py
@@ -7,9 +7,14 @@ class Settings():
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         self.ship_speed_factor = 3.5
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # fleet_direction = 1 обозначает движение вправо; а -1 - влево.
+        self.fleet_direction = 1
         # Bullet parameters
         self.bullet_speed_factor = 2.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 3
+
